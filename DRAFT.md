798a1c01e59f6967f51cd9fb1ad1e265c51d8eca
Dmtrs Flaco Mengidis <tydeas.dr@gmail.com>
2019-05-14 23:02:15 +0300

    feat(min/max): Added frequency min/max and drop of messages

--- DRAFT.md
+++ DRAFT.md
@@ -6,11 +6,11 @@ Quick notes.
 - Clear entries after time / maxSize for buffer as sesitivity argument.
   Initialize the runtime with give value from background. On change of sensitivy value update content-scripts (?). 
   Sensitivity is maxSize of buffer and interval of flush.
-- Frequency barriers. 
 - ADSR utilizing breakdown found in performance entry.
 - Utilize periodic waves to create unique osc for each domain/path or name.
 
 ##Resources
+
 ###Periodic waves
 - [wave-tables repository](https://github.com/mohayonao/wave-tables)
 - [BaseAudioContext.createPeriodicWave](https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createPeriodicWave)
--- nix-background.js
+++ nix-background.js
@@ -3,6 +3,9 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const DURATION_SCALE = 1000;
 const FREQUENCY_SCALE = 2;
 
+const FREQUENCY_MAX = 22100;
+const FREQUENCY_MIN = 20;
+
 const durationScale = message => message.duration / DURATION_SCALE;
 const frequencyCalc = message => {
   if (message.transferSize > 0) {
@@ -11,8 +14,22 @@ const frequencyCalc = message => {
   return message.decodedBodySize / FREQUENCY_SCALE;
 };
 
+const stats = { hits: 0, totals: 0 };
+setInterval(({ hits, totals }) => {
+  console.info(`${Math.round((hits/totals)*100)}% of ${totals}`)
+}, 4000, stats);
+
 const onMessageListener = (ctx) => (message, sender) => {
+  stats.totals++;
+  
   let frequency = frequencyCalc(message);
+  if (frequency > FREQUENCY_MAX || frequency < FREQUENCY_MIN) {
+    console.warn(`drop ${message.name} freq ${frequency}`);
+    return;
+  }
+
+  stats.hits++;
+
   let options = {
     type: 'sine',
     detune: 0,
@@ -26,7 +43,6 @@ const onMessageListener = (ctx) => (message, sender) => {
   let duration = durationScale(message);
   let endTime = ctx.currentTime + duration;
   osc.stop(endTime);
-  console.log(message.name, options.frequency, duration);
 };
 
 browser
